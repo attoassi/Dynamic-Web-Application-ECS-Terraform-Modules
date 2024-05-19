2b25a131962b4a0d29ce13e4f5812533e1b54ad6
attoassi <122966827+attoassi@users.noreply.github.com>
2024-05-19 01:38:00 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket = #"Your-bucket-name"
-    key    = #"Your-key-name"
+    bucket = "Your-bucket-name"
+    key    = "Your-key-name"
 
     region         = "us-east-1"
-    profile        = #"Your-profile-name"
-    dynamodb_table = #"Your-db-table-name"
+    profile        = "Your-profile-name"
+    dynamodb_table = "Your-db-table-name"
   }
 }
 
